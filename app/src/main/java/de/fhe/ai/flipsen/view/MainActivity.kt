86e2167403f57e7463bab866adfc157b9ce720e7
DevTobias <tobi.kaerst@gmx.de>
2022-12-07 19:57:46 +0100

    feat: changed branding

--- app/src/main/java/de/fhe/ai/flipsen/view/MainActivity.kt
+++ app/src/main/java/de/fhe/ai/flipsen/view/MainActivity.kt
@@ -1,8 +1,12 @@
 package de.fhe.ai.flipsen.view
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -20,6 +24,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        window.statusBarColor = ContextCompat.getColor(this, R.color.neutral_500)
+
         val navView: BottomNavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         val appBarConfiguration = AppBarConfiguration(setOf(
@@ -28,5 +34,15 @@ class MainActivity : AppCompatActivity() {
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.custom_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Toast.makeText(this, "Not implemented yet...", Toast.LENGTH_LONG).show()
+        return super.onOptionsItemSelected(item)
     }
 }
