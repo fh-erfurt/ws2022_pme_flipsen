6f95631cb0b99ea941b28e4b6549e8cd9726a7ff
DevTobias <tobi.kaerst@gmx.de>
2022-12-12 15:20:13 +0100

    feat: fixed login page

--- app/src/main/java/de/fhe/ai/flipsen/view/ui/login/LoginActivity.kt
+++ app/src/main/java/de/fhe/ai/flipsen/view/ui/login/LoginActivity.kt
@@ -4,9 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import dagger.hilt.android.AndroidEntryPoint
 import de.fhe.ai.flipsen.R
 import de.fhe.ai.flipsen.view.MainActivity
 
+
+@AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
